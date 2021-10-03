7c5a2bda61ada6f7c3eb13d21f56e52d0841c474
Srishti-Goel <43054441+Srishti-Goel@users.noreply.github.com>
2021-10-03 20:59:17 +0530

    Update calc_risk.py

--- FinalRiskCalculator/calc_risk.py
+++ FinalRiskCalculator/calc_risk.py
@@ -2,16 +2,20 @@ from learning import model, max_values, train_X, train_Y, test_X, test_Y, sigmoi
 import numpy as np
 import sys
 
-NDVI = 1
-LST = 35
-thermal_anomalies = 1
+NDVI = float(sys.argv[1])
+LST = float(sys.argv[2])
+thermal_anomalies = float(sys.argv[3])
 
 logistic_reg_model = model(train_X, train_Y, test_X, test_Y, num_iterations= 1000, learning_rate = 0.005, print_cost = False)
 X = np.array([[NDVI], [LST], [thermal_anomalies]])
-X = X / max_values
+X[0] = X[0] / max_values[0][0]
+
+X[1] = X[1] / max_values[1][0]
+
+X[2] = X[2] / max_values[2][0]
 A = sigmoid( np.dot(logistic_reg_model['w'].T, X) + logistic_reg_model['b'] )
 A = np.squeeze(np.array(A))
-risk = ''
+risk = 'Bleh'
     
 if A < 0.33:
     risk = 'GREEN'
