5cd11e122534590f655eb6f5dd600c72afa2c690
Srishti-Goel <43054441+Srishti-Goel@users.noreply.github.com>
2021-12-28 19:14:49 +0530

    Fixed Calculator mistake

--- FinalRiskCalculator/app.js
+++ FinalRiskCalculator/app.js
@@ -1,37 +1,35 @@
 const express = require("express");
-const bodyParser = require("body-parser")
-const app = express();
+const bodyParser = require("body-parser");
 const spawn = require("child_process").spawn;
+
+const router = express.Router();
   
-app.use(bodyParser.urlencoded({extended:true}));
+router.use(bodyParser.urlencoded({extended:true}));
 
-app.get("/", function(req, res){
-  res.sendFile(__dirname + "/index.html");
+router.get("/", function(req, res){
+  res.sendFile(__dirname + "/calculator.html");
 })
 
-app.post("/", function(req,res){
+router.post("/", function(req,res){
 
-  console.log("Going to python");
-  console.log(req.body);
-    const calculateNDVIProcess = spawn('python', ['./modis.py']);
-    calculateNDVIProcess.on('data', (data) => {
-      console.log('From MODIS: ', data.toString());
-    })
-
-
-    NDVI = 1;
-    LST = 35;
-    ta = 1;
-
-    const pythonProcess = spawn('python', ['./calc_risk.py', NDVI, LST, ta]);
+  console.log("Going to python with", req.body);
+  const pythonProcess = spawn('python', ['./modis.py', req.body.lat, req.body.long]);
     pythonProcess.stdout.on('data', (data) => {
-    console.log(data.toString());
-    res.write(data);
-    res.send();
+      dataStr = data.toString().split('\r\n');
+      console.log("Param data: ", dataStr);
+      NDVI = (parseFloat(dataStr[0]));
+      LST = (parseFloat(dataStr[1]));
+      ta = (parseFloat(dataStr[2]));
+
+      const pythonProcess2 = spawn('python', ['./calc_risk.py', NDVI, LST, ta]);
+      pythonProcess2.stdout.on('data', (data) => {
+      dataSTR = data.toString();
+      res.write(data);
+      res.send();
+
+    })    
 });
-    
+  
 })
 
-app.listen(3000, function(){
-  console.log("Server is running on port 3000.");
-})
+module.exports = router;
